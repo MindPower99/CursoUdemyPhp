19f7c4428409b2d9ef5c2b9b6b3d422a8c4c34a5
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-10-13 18:01:44 -0300

    Tipos de Dados: Integer e Float

--- numeros.php
+++ numeros.php
@@ -15,7 +15,7 @@
 		<h3>Operadores Aritméticos</h3>
 
 		<h4>Escreva a operação:</h4>
-			<p><?php  ?></p>
+			<p><?php echo 2+7; ?></p>
 			<br>
 		
 		
@@ -29,6 +29,7 @@
 
 			// Cálculo do total
 
+			$total = $quant * $preco;
 		?>
 
 			<table>
@@ -39,10 +40,10 @@
 					<th>Total</th>
 				</tr>
 				<tr>
-					<td><?php  ?></td>
-					<td><?php  ?></td>
-					<td><?php  ?></td>
-					<td><?php  ?></td>
+					<td><?php  echo $cod; ?></td>
+					<td><?php  echo $quant; ?></td>
+					<td><?php  echo number_format($preco, 2, ",","."); ?></td>
+					<td><?php  echo number_format($total, 2, ",","."); ?></td>
 				</tr>
 			</table>
 
@@ -54,16 +55,21 @@
 			?>
 
 			<h4>Arredondar:</h4>
-			<p><?php  ?></p>
+			<p><?php  echo round($num1, 3);?></p>
 			<br>
 
 			<h4>Arredondar para cima:</h4>
-			<p><?php  ?></p>
+			<p><?php  echo ceil($num1); ?></p>
 			<br>
 
 			<h4>Arredondar para baixo:</h4>
-			<p><?php  ?></p>
+			<p><?php  echo floor($num1); ?></p>
 			<br>
+
+			<h4>Raiz quandrada:</h4>
+			<p><?php  echo sqrt($num2); ?></p>
+			<br>
+
 
 
 
